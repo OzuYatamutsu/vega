96fdd5f1744b0ec997fc6f9e5261d6848fe9da21
Sean Collins <seanc@spiceworks.com>
2016-03-20 17:12:20 -0500

    Use new taskrunner

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
+'require strict';
+
+var Taskrunner = require('./taskrunner_base');
+
 var fs = require('fs');
 var webdriverio = require('webdriverio');
 var selenium = require('selenium-standalone');
 var client = ""; // WebdriverIO client
 
 function main() {
-  client
-    .url("https://milestonecorporate.residentportal.com/resident_portal/?module=authentication&action=view_login")
-    .setValue("#email", "seanc@seancotech.com")
-    .setValue("#password", "NS141faw")
-    .click("#loginVerify")
-    .click(".closeLightbox")
-    .end();
-}
-
-function seleniumPrep() {
-  selenium.install(() => {
-    selenium.start(() => {
-      client = webdriverio.remote();
-      client.init().then(() => main());
-    });
-  });
+  // DEBUG
+  var Taskrunner = new Taskrunner("tasks/check_if_apt_payment_due.task");
+  Taskrunner.run();
 }
 
 if (require.main === module) {
-    seleniumPrep();
+  main();
 }
