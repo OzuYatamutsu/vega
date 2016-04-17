51206bf462e36b22280c76fe17e8f630da7aac98
Sean Collins <seanc@seancotech.com>
2016-04-17 15:26:34 -0500

    Calendar parsing module

---
+++ calendars.py
@@ -0,0 +1,9 @@
+## Config: Put URLs to ICS endpoints in the array below.
+## e.g. calendar_urls = [
+##  "https://path-to-cal.com/calendar.ics",
+##  "http://path-to-second-cal.com/?cal=my_cal",
+## ]
+
+calendar_urls = [
+    "https://sharing.calendar.live.com/calendar/private/0371bb7d-512f-42d8-94c5-56130e91f297/c347fe96-bdb4-4036-869e-bccd87eea741/cid-85f9fbf217988a6f/calendar.ics"
+]
