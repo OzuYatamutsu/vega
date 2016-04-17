1b7d48583cc7d17daa14709426e0e3b315e27f92
Sean Collins <seanc@seancotech.com>
2016-04-17 16:20:32 -0500

    Timestamp

--- strings.py
+++ strings.py
@@ -1,4 +1,6 @@
-I_FLAG = "[INFO] "
+from datetime import datetime
+
+I_FLAG = str(datetime.now()) + " [INFO] "
 I_GET_BROWSER_PROG = I_FLAG + "Checking for %s support."
 I_GET_BROWSER_SUPPORT = I_FLAG + "Found %s in PATH!"
 I_GET_BROWSER_NSUPPORT = I_FLAG + "%s was not found."
@@ -8,8 +10,8 @@ I_CAL_UPDATE_INSERT = I_FLAG + "Pushing event: %s - %s"
 I_TASK_RUN = I_FLAG + "Running task: %s"
 I_TASK_RUN_COMPLETE = I_FLAG + "Task complete: %s"
 
-W_FLAG = "[WARN] "
+W_FLAG = str(datetime.now()) + " [WARN] "
 
-E_FLAG = "[ERROR] "
+E_FLAG = str(datetime.now()) + " [ERROR] "
 E_GET_BROWSER_NONE = E_FLAG + """No compatible browsers were found in PATH! 
 Please install one of the following:"""
