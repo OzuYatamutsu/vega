df64f84c996cd5af82f12e38fc066a02e1a0b778
Sean Collins <seanc@seancotech.com>
2016-04-18 22:58:20 -0500

    Fix browser init

--- helpers.py
+++ helpers.py
@@ -3,22 +3,23 @@ from strings import *
 
 '''Returns the first available browser which can be used as a driver.'''
 def get_first_available_browser(verbose=False):
-    browsers = {
-        'PhantomJS': lambda: webdriver.PhantomJS(),
-        'Chrome': lambda: webdriver.Chrome(),
-        #'Firefox': lambda: webdriver.Firefox(), 
-        'Opera': lambda: webdriver.Opera(),
-        'Ie': lambda: webdriver.Ie()
-    }
-   
+    browsers = [
+        ('PhantomJS', webdriver.PhantomJS),
+        ('Chrome', webdriver.Chrome),
+        #('Firefox', webdriver.Firefox), 
+        ('Opera', webdriver.Opera),
+        ('Ie', webdriver.Ie)
+    ]
+    
     for browser in browsers:
         try:
-            if verbose: print(I_GET_BROWSER_PROG % browser)
-            browsers[browser]().quit()
-            print(I_GET_BROWSER_SUPPORT % browser)
-            return browsers[browser]()
+            if verbose: print(I_GET_BROWSER_PROG % browser[0])
+            result = browser[1]()
+            print(I_GET_BROWSER_SUPPORT % browser[0])
+            return result
         except:
-            print(I_GET_BROWSER_NSUPPORT % browser)
+            print(I_GET_BROWSER_NSUPPORT % browser[0])
+            continue
     print(E_GET_BROWSER_NONE)
-    for browser in browsers: print(browser)
+    for browser in browsers: print(browser[0])
     return None
