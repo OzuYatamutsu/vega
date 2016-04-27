b64d2cb6f63ee1ff45342c409379816ea0751ad9
Sean Collins <seanc@seancotech.com>
2016-04-27 12:49:49 -0500

    Commit first task

--- tasks/check_if_apt_payment_due.py
+++ tasks/check_if_apt_payment_due.py
@@ -1,8 +1,27 @@
 ## Checks if my apartment payment is due.
 from tasks.browser_task import BrowserTask
 
-def run():
-    print("TODO: Stubbed test")
+def run(browser):
+    url = "https://milestonecorporate.residentportal.com/resident_portal/"
+    print("Navigating.")
+    browser.get(url)
+    
+    email, password = get_cred(url)
+    print("Setting email.")
+    set_value(browser, '#email', email)
+    print("Setting password.")
+    set_value(browser, '#password', password)
+    
+    print("Submitting form.")
+    browser.find_element_by_id('authentication').submit()
+    
+    print("Waiting...")
+    wait_for_class(browser, 'modulePay')
+    
+    print("Getting value.")
+    browser.execute_script("$('strong > span').attr('id', 'tar')")
+    text = get_text_from_id(browser, 'tar')
+    print("Amount due: " + text)
     return 1
     
 task = BrowserTask(run_func = run)
