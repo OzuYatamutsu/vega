f46b1f788e25019245ed3abdade0b2cc13e8fa0c
Sean Collins <seanc@seancotech.com>
2016-04-29 12:47:42 -0500

    Humanize from template

---
+++ jinhaize.py
@@ -0,0 +1,17 @@
+## Constructs an HTML string from a template, result, and color.
+from db_commit import pull_data
+from time import ctime
+
+QUERY = "SELECT timestamp, result, humanized_template, color FROM TaskResult WHERE task_file = '%s' LIMIT 1"
+
+def jinhaize(task_file):
+    data = pull_data(task_file)[0]
+    
+    timestamp = ctime(float(data[0]))
+    result = data[1]
+    humanized_template = data[2]
+    color = data[3]
+    
+    return (humanized_template % \ 
+        ('<font color="%s">%s</font>' % (color, result)) \
+        ) + (" (Last updated %s" % timestamp)
