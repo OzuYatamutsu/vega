4bfae394b8ddd81fc800f38367571ae85438ff14
Sean Collins <seanc@seancotech.com>
2016-05-07 17:53:30 -0500

    Check if electric bill is due

---
+++ tasks/check_if_electric_bill_due.py
@@ -0,0 +1,31 @@
+## Checks if my electricity bill is due.
+from tasks.browser_task import BrowserTask
+
+HUMAN_STR = "Your electricity bill is %s."
+
+def run(browser):
+    url = "https://coautilities.com/wps/myportal/occ/login"
+    print("Navigating.")
+    browser.get(url)
+    
+    email, password = get_cred(url)
+    print("Setting email.")
+    set_value(browser, "[name='username']", email)
+    print("Setting password.")
+    set_value(browser, "[name='password']", password)
+    
+    print("Submitting form.")
+    browser.find_element_by_id('LoginForm').submit()
+    
+    print("Waiting...")
+    wait_for_class(browser, 'container12')
+    
+    print("Getting value.")
+    browser.execute_script("$('.occ_value.occ_bodyBold').attr('id', 'tar')")
+    text = get_text_from_id(browser, 'tar')
+    return (text, "green" if float(text.replace("$", "").replace(",", "")) <= 0.0 else "red")  
+    print("Amount due: " + text)
+    
+task = BrowserTask(run_func = run, humanized_template = HUMAN_STR)
+task.run()
+task.commit_result()
