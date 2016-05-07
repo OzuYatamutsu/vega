63f8e4e23775ad8cdd2f191240f44b87a9824d21
Sean Collins <seanc@seancotech.com>
2016-05-06 20:25:41 -0500

    Bump up default timeout

--- browser_utils.py
+++ browser_utils.py
@@ -7,17 +7,18 @@ def inject_jquery_if_needed(browser):
     browser.execute_script('''if (typeof(jQuery) === "undefined") { 
     var script = document.createElement("script"); 
     script.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";
+    script.id = "vega-jq";
     document.getElementsByTagName("head")[0].appendChild(script);
     }''')
 def set_text(browser, selector, text):
-    inject_jquery_if_needed(browser);
+    inject_jquery_if_needed(browser)
     browser.execute_script("""jQuery("%s").text("%s")""" % (selector, text))
 def set_value(browser, selector, value):
-    inject_jquery_if_needed(browser);
+    inject_jquery_if_needed(browser)
     browser.execute_script("""jQuery("%s").val("%s")""" % (selector, value))
-def wait_for_id(browser, id, timeout=10):
+def wait_for_id(browser, id, timeout=60):
     WebDriverWait(browser, timeout).until(lambda browser: browser.find_element_by_id(id))
-def wait_for_class(browser, class_name, timeout=10):
+def wait_for_class(browser, class_name, timeout=60):
     WebDriverWait(browser, timeout).until(lambda browser: browser.find_element_by_class_name(class_name))
 def get_text_from_id(browser, id):
     return browser.find_element_by_id(id).text
