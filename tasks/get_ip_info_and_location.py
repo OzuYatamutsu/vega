9fbf39a49268b154cf48770755402dc0a70f7940
Sean Collins <seanc@seancotech.com>
2016-05-11 18:05:44 -0500

    Fix task

--- tasks/get_ip_info_and_location.py
+++ tasks/get_ip_info_and_location.py
@@ -7,13 +7,13 @@ HUMAN_STR = "Our current location is %s. Current weather: %s."
 
 def run():
     WUNDERGROUND_API_LOC = "http://api.wunderground.com/api/%s/conditions/q/autoip.json"
-    api_key = get_cred(WUNDERGROUND_API_LOC)["password"]
+    api_key = get_cred(WUNDERGROUND_API_LOC)[1]
     result = get(WUNDERGROUND_API_LOC % api_key).json()
     
     location = result["current_observation"]["observation_location"]["full"]
     conditions = result["current_observation"]["weather"]
-    temperature_c = result["current_observation"]["temp_c"] + "\xb0C"
-    temperature_f = result["current_observation"]["temp_f"] + "\xb0F"
+    temperature_c = str(result["current_observation"]["temp_c"]) + "\xb0C"
+    temperature_f = str(result["current_observation"]["temp_f"]) + "\xb0F"
 
     text = location + RESULT_SEP_TOKEN + ("%s, %s" % (conditions, temperature_c))
     return (text, "blue")
