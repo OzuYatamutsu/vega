ffcee0f76ad1d0327287d9d8d7811b05e16e0f6b
Sean Collins <seanc@seancotech.com>
2016-05-13 00:30:24 -0500

    Clear TaskResult table before run + imports

--- taskrunner.py
+++ taskrunner.py
@@ -1,6 +1,7 @@
 ## Runs all tasks and pushes the result to data.sqlite.
 from os import getcwd, sep, listdir
 from selenium import *
+from datetime import datetime, time
 
 from strings import *
 from ical_parser import get_ical
@@ -14,6 +15,7 @@ TASK_DIR = getcwd() + sep + "tasks"
 # Queries
 DELETE_CAL = "DELETE FROM Calendar"
 UPDATE_CAL = "INSERT INTO Calendar(start_time, description) VALUES (?, ?)"
+DELETE_TASKS = "DELETE FROM TaskResult"
 
 # Task classes
 IGNORE = ["task.py", "browser_task.py", "script_task.py"]
@@ -35,7 +37,8 @@ def update_calendars():
     return True
     
 def update_tasks():
-    for task in [item for item in listdir(TASK_DIR) if item.endswith('.py') and item not in IGNORE]:
+    push_data(DELETE_TASKS)
+    for task in sorted([item for item in listdir(TASK_DIR) if item.endswith('.py') and item not in IGNORE]):
         print(I_TASK_RUN % task)
         path = TASK_DIR + sep + task
         try:
