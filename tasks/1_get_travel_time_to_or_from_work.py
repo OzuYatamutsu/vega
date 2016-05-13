64f4fef0dda66cb446f279f044355ab65ff448e7
Sean Collins <seanc@seancotech.com>
2016-05-13 09:46:42 -0500

    Get actual traffic delay + get order correct

--- tasks/1_get_travel_time_to_or_from_work.py
+++ tasks/1_get_travel_time_to_or_from_work.py
@@ -4,24 +4,24 @@ from strings import RESULT_SEP_TOKEN
 from tasks.script_task import ScriptTask
 from requests import get
 
-HUMAN_STR_1 = "Right now, it'll take about %s to get from home to work."
-HUMAN_STR_2 = "Right now, it'll take about %s to get from work to home."
+HUMAN_STR_1 = "Right now, it'll take about %s to get from <b>home</b> to <b>work</b> (with traffic)."
+HUMAN_STR_2 = "Right now, it'll take about %s to get from <b>work</b> to <b>home</b> (with traffic)."
 DIRECTION_SWITCH_THRESH = time(12, 0)
 HUMAN_STR = HUMAN_STR_1 if DIRECTION_SWITCH_THRESH > datetime.time(datetime.now()) else HUMAN_STR_2
 
 def run():
-    BASE_URL = "https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s"
+    BASE_URL = "https://maps.googleapis.com/maps/api/directions/json?key=%s&departure_time=now&mode=driving&origin=%s&destination=%s"
     DIRECTION_SWITCH_THRESH = time(12, 0)
     addresses, api_key = get_cred(BASE_URL)
     addresses = addresses.split(RESULT_SEP_TOKEN) # Home, work
     travel_time = {}
     if DIRECTION_SWITCH_THRESH > datetime.time(datetime.now()):
         print("Getting travel time to work.")
-        travel_time = get(BASE_URL % tuple(list(reversed(addresses)))).json()
+        travel_time = get(BASE_URL % ((api_key,) + tuple(addresses))).json()
     else:
         print("Getting travel time to home.")
-        travel_time = get(BASE_URL % tuple(addresses)).json()
-    travel_time = travel_time["routes"][0]["legs"][0]["duration"]["text"]
+        travel_time = get(BASE_URL % ((api_key,) + tuple(list(reversed(addresses))))).json()
+    travel_time = travel_time["routes"][0]["legs"][0]["duration_in_traffic"]["text"] 
     print("Travel time is %s." % travel_time)
     return (travel_time, "blue")
     
